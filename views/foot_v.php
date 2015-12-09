b57ba1226726e08994150d96c3d25eca61a7eabf
molipate <antoine.febvre1@gmail.com>
2015-12-09 01:25:49 +0100

    Beginning of admin space

--- views/foot_v.php
+++ views/foot_v.php
@@ -1,14 +1,12 @@
     <div class="admin">
-        <div class="admin_onglet">
-            <?php
-            if($_SESSION['connexion'] == "true") {
-                echo '<a href="'.BASE_URL.'index.php/admin/index'.'">Deconnexion</a>';
-                echo '<a href="'.BASE_URL.'index.php/admin/index'.'">Gestion du site</a>';
-            }
-            else
-                echo '<a href="'.BASE_URL."index.php/admin/index".'">Administration</a>';
-            ?>
-        </div>
+        <?php
+        if($_SESSION['connexion'] == "true") {
+            echo '<a href="'.BASE_URL.'index.php/admin/index'.'">Gestion du site</a>';
+            echo '<a href="'.BASE_URL.'index.php/admin/index'.'">Deconnexion</a>';
+        }
+        else
+            echo '<a href="'.BASE_URL."index.php/admin/index".'">Administration</a>';
+        ?>
     </div>
     </body>
 </html>
