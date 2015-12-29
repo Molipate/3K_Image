8a51292c195fd2c438e16c859ceae681e623b020
molipate <antoine.febvre1@gmail.com>
2015-12-29 19:15:03 +0100

    try to commit something

--- views/index_v.php
+++ views/index_v.php
@@ -6,7 +6,25 @@
     </head>
     <body>
 
-        <img src="<?= BASE_IMG ?>logo.png" alt="">
+        <svg height="500" width="450">
+
+            <a xlink:href="<?=BASE_URL?>index.php/main/connexion">
+                <polygon points="225,0,258,156,0,390"></polygon>
+            </a>
+
+            <a xlink:href="<?=BASE_URL?>index.php/video/listeCategorie">
+                <polygon points="225,0,298,341,450,390"></polygon>
+            </a>
+
+            <a xlink:href="<?=BASE_URL?>index.php/main/association">
+                <polygon points="0,390,310,390,298,341,118,283"></polygon>
+            </a>
+
+            <a xlink:href="">
+                <image xlink:href="<?=BASE_IMG?>name.png" x="0" y="440" height="37px" width="450px"></image>
+            </a>
+
+        </svg>
 
     </body>
 </html>
