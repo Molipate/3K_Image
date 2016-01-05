c1d9b3cd1daf981879a95e8b87b405299840bed5
molipate <antoine.febvre1@gmail.com>
2016-01-05 18:47:31 +0100

    video system over soon maybe ??

--- views/index_v.php
+++ views/index_v.php
@@ -11,13 +11,13 @@
             <a xlink:href="<?=BASE_URL?>index.php/video/index">
                 <title>Visionnes nos vidéos !</title>
                 <polygon points="225,0,258,156,0,390"></polygon>
-                <text x="150" y="150" fill="black">Nos vidéos !</text>
+                <text x="150" y="150" fill="black">Nos vidéos</text>
             </a>
 
             <a xlink:href="<?=BASE_URL?>index.php/main/contact">
                 <title>Contactez nous !</title>
                 <polygon points="225,0,298,341,450,390"></polygon>
-                <text x="310" y="320">Contacts !</text>
+                <text x="310" y="320">Contacts</text>
             </a>
 
             <a xlink:href="">
@@ -27,7 +27,7 @@
             </a>
 
             <a xlink:href="<?=BASE_URL?>index.php/main/association">
-                <title>Mieux nous connaître !</title>
+                <title>Mieux nous connaître</title>
                 <image xlink:href="<?=BASE_IMG?>name.png" x="0" y="440" height="37px" width="450px"></image>
             </a>
 
