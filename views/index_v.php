739a04c051f0d2ef368b8bc74d71c5b62d54b8f6
root <root@debian>
2016-01-21 09:02:15 +0100

    update visuelle

--- views/index_v.php
+++ views/index_v.php
@@ -5,32 +5,36 @@
         <link rel="stylesheet" href="<?=BASE_URL?>assets/css/index.css">
         <script src="<?=BASE_URL?>assets/js/jquery-1.12.0.min.js"></script>
         <script src="<?=BASE_URL?>assets/js/jQueryRotate.js"></script>
+
+        <script src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js'></script>
     </head>
     <body>
 
-        <svg height="500" width="450">
+        <svg height="390" width="500">
 
             <a href="<?=BASE_URL?>index.php/main/videos" >
                 <title>Visionnes nos vidéos !</title>
-                <polygon  points="225,0,258,156,0,390" id="p_video"></polygon>
-                <text x="150" y="150" fill="black">Nos vidéos</text>
+                <polygon  points="275,0,308,156,50,390" id="p_video"></polygon>
+                <text x="200" y="150" fill="black">Nos vidéos</text>
             </a>
 
             <a href="<?=BASE_URL?>index.php/main/contact"  >
                 <title>Contactez nous !</title>
-                <polygon  points="225,0,298,341,450,390" id="p_contact"></polygon>
-                <text x="310" y="320">Contacts</text>
+                <polygon  points="275,0,348,341,500,390" id="p_contact"></polygon>
+                <text x="360" y="320">Contacts</text>
             </a>
+
 
             <a href="" >
                 <title>Rejoignez nous !</title>
-                <polygon  points="0,390,310,390,298,341,118,283" id="p_rejoindre"></polygon>
-                <text x="100" y="350">Nous rejoindre !</text>
+                <polygon  points="50,390,360,390,348,341,168,283" id="p_rejoindre"></polygon>
+                <text x="150" y="350">Nous rejoindre !</text>
             </a>
-
+        </svg>
+        <svg height="110" width="500">
             <a href="<?=BASE_URL?>index.php/main/association" id="link" >
                 <title>Mieux nous connaître</title>
-                <image xlink:href="<?=BASE_IMG?>name.png" x="0" y="440" height="37px" width="450px"></image>
+                <image xlink:href="<?=BASE_IMG?>name.png" x="50" y="60" height="37px" width="450px"></image>
             </a>
 
         </svg>
@@ -45,6 +49,8 @@
 
             $("a").click(function(){
 
+            	$("svg:eq(0) text").fadeOut();
+
                 $("#p_video").rotate({
                     angle: 0,
                     center: ["0%", "100%"],
@@ -52,18 +58,34 @@
                 });
                 $("#p_contact").rotate({
                     angle: 0,
-                    center: ["100%", "100%"],
+                    center: ["44%", "87%"],
                     animateTo:-60
                 });
+               
+                
                 $("#p_rejoindre").rotate({
                     angle: 0,
-                    animateTo:180
+                    center: ["0%", "100%"],
+                    animateTo:-17.3
                 });
+                
+                setTimeout(function(){
+                	
+                    
+                    $("svg").eq(0).slideUp(500);
+                    //$("svg").slideUp();
+                    setTimeout(function(){
+                    
+                    $("svg").eq(1).fadeOut(500);
+                    //$("svg").slideUp();
 
+                	},500);
+                },1000);
+                
                 var href = $(this).attr('href');
 
                 // Delay setting the location for one second
-                setTimeout(function() {window.location = href}, 2000);
+                setTimeout(function() {window.location = href}, 2500);
                 return false;
             });
 
